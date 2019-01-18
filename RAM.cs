684d44ab4cd7497add70e15884c6491df6fb88e4
tpinto7 <43225579+tpinto7@users.noreply.github.com>
2019-01-17 20:15:29 -0500

    Add files via upload

---
+++ RAM.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Bme121
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteLine( CalculateMRAM( 0, PI, 4, Sine ) );
+			double Sine( double val ){ return Sin(val); } 
+        }
+        
+        public delegate double Function( double val );
+        
+        
+        static double CalculateLRAM( double start, double end, double intervals, Function func )
+        {
+			double ram = 0;
+			double delta = (end - start)/intervals;
+			for( int i = 0; i < intervals; i ++ )
+			{
+				double xValue = start;
+				xValue += delta*i;
+				
+				ram += delta*func( xValue );
+			}
+			return ram;
+		}
+        
+        static double CalculateRRAM( double start, double end, double intervals, Function func )
+        {
+			double ram = 0;
+			double delta = (end - start)/intervals;
+			for( int i = 0; i < intervals; i ++ )
+			{
+				double xValue = start;
+				xValue += delta*i;
+				xValue += delta;
+				
+				ram += delta*Sin( xValue );
+			}
+			return ram;
+		}
+		
+		static double CalculateMRAM( double start, double end, double intervals, Function func )
+        {
+			double ram = 0;
+			double delta = (end - start)/intervals;
+			for( int i = 0; i < intervals; i ++ )
+			{
+				double xValue = start;
+				xValue += delta*i;
+				xValue += delta/2;
+				
+				ram += delta*Sin( xValue );
+			}
+			return ram;
+		}
+        
+        
+        static double CalculateLowerRAM( double start, double end, double intervals, Function func )
+        {
+			double ram = 0;
+			double delta = (end - start)/intervals;
+			for( int i = 0; i < intervals; i ++ )
+			{
+				double xValue = start;
+				xValue += delta*i;
+				
+				if( xValue >= PI/2 ) xValue += delta;
+				
+				ram += delta*Sin( xValue );
+			}
+			return ram;
+		}
+		
+		static double CalculateUpperRAM( double start, double end, double intervals, Function func )
+        {
+			double ram = 0;
+			double delta = (end - start)/intervals;
+			for( int i = 0; i < intervals; i ++ )
+			{
+				double xValue = start;
+				xValue += delta*i;
+				
+				if( xValue < PI/2 ) xValue += delta;
+				
+				
+				ram += delta*Sin( xValue );
+			}
+			return ram;
+		}
+		
+    }
+}
